f0b3c85f30ff00ad7ac45b2b0740efec4a9036e3
Yingyin <105791120@live.asia.edu.tw>
2016-12-26 15:29:47 +0800

    ex02 105791120 杨钰莹

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,4 +1,7 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
  * Date: 2016/12/26
@@ -8,8 +11,17 @@
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca=new Scanner(System.in);
+	String str =sca.nextLine();
+	char v2=str.charAt(0);
+	int v1=(int)v2;
+	int sum=0;
+	for(int i=0;i<str.length();i++){
+		sum+=(int) str.charAt(i);
+		
+	}
+		
+	System.out.println(sum);
 	}
 
 }
